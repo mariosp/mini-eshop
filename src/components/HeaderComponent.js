f57947c5c1586ffd666f4b98bf1e326e3e0404ed
Marios <mariospatsis@gmail.com>
2020-02-24 21:47:27 +0200

    Init, Main and Header Components

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,30 @@
+import React,{useState} from "react";
+import {Collapse, Nav, Navbar, NavbarBrand, NavbarText, NavbarToggler, NavItem, NavLink} from 'reactstrap'
+
+
+const Header = ()=>{
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">Mini e-shop</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink>Products</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink>Contact us</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    )
+};
+
+export default Header;
